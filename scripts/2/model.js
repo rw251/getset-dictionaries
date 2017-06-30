59706ef8d67de6d5bb501261f2617a44ea3fd650
Richard Williams <richard.williams2@manchester.ac.uk>
2017-06-30 10:34:34 +0100

    Ancestors should be an array of strings with an index

--- scripts/2/model.js
+++ scripts/2/model.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const CodeSchema = new Schema({
   _id: String, // The clinical code
   t: String, // A | separated list of
-  a: String, // Ancestors in a comma delimited string
+  a: { type: [String], index: true }, // Ancestors in a comma delimited string
   p: { type: [String], index: true }, // Immediate parents in a comma delimited string
   c: { type: [String], index: true }, // An array of strings of length n contained in t
 });
